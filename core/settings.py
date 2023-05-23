e882731d6fae45aba84f3f18b359160c37d6bb9f
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-23 14:09:31 +0500

    add app

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'lms.apps.LmsConfig',
+    'enrollments.apps.EnrollmentsConfig'
 
     'rest_framework',
 ]
